031e4bb539d00f0727d00341e4fbf5603795bb42
Viktor Holta <viktor.holta@outlook.com>
2024-01-26 20:56:21 +0100

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import './index.css';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
