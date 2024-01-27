bbce05d38c525a14923da0d6ff27084a6abf827e
Viktor Holta <viktor.holta@outlook.com>
2024-01-27 16:35:11 +0100

    Update GameCard design

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,16 +5,17 @@ type GameCardData = {
 };
 
 function GameCard({ data }: { data: GameCardData }) {
-  console.log(data.image)
   return (
-    <div className="rounded-lg overflow-hidden bg-red-400">
-      <img className="object-cover w-full h-52" src={data.image} />
-      <div className="p-2 grid grid-cols-gamecard">
-        <span className="font-bold">{data.name}</span>
-        <span>${data.price}</span>
-        <button className="col-start-2 row-start-1 row-end-3 px-6 rounded hover:bg-slate-50">
-          Buy
-        </button>
+    <div className="pr-3 pt-3 rounded-3xl bg-[#707070]">
+      <div className="rounded-xl overflow-hidden bg-[#ccc]">
+        <img className="object-cover w-full h-52" src={data.image} />
+        <div className="p-2 grid grid-cols-gamecard">
+          <span className="font-bold">{data.name}</span>
+          <span>${data.price}</span>
+          <button className="col-start-2 row-start-1 row-end-3 px-6 rounded bg-[#707070]">
+            Buy
+          </button>
+        </div>
       </div>
     </div>
   );
