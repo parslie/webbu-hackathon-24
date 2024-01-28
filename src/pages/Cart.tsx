ca4edf8b161fa5bdfce3f32a4b2af83f5ac764e5
SaraHQ <sara.holmquist@gmail.com>
2024-01-28 12:29:05 +0100

    sista

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
-import { getCart, type CartItem, setCartItemCount, buyCartItems } from "../api/cart";
+import {
+  getCart,
+  type CartItem,
+  setCartItemCount,
+  buyCartItems,
+} from "../api/cart";
 import { useNavigate } from "react-router-dom";
 
-function CartItemCard({ data, updateCart }: { data: CartItem, updateCart: () => void }) {
+function CartItemCard({
+  data,
+  updateCart,
+}: {
+  data: CartItem;
+  updateCart: () => void;
+}) {
   const onSetCount = (n: number) => {
     setCartItemCount(data.game, n);
     updateCart();
   };
-  
   return (
-    <div className="flex flex-row gap-2 even:bg-slate-200 odd:bg-white">
-      <img className=""
-           src={data.game.image}
-           alt="Box art" />
-      <span className="flex-grow">{data.game.name}</span>
-      <span>${data.game.price}</span>
+    <div className="flex flex-auto even:bg-slate-200 odd:bg-white border-4 max-w-lg">
+      <img className="h-20" src={data.game.image} alt="Box art" />
+      <span className=" mx-24 text-center self-center">{data.game.name}</span>
+      <span className="self-center">${data.game.price}</span>
       <span className="self-center">×</span>
-      <input type="number" value={data.count} 
-             onChange={(e) => onSetCount(parseInt(e.target.value))}
-             className="w-[3ch] text-center" />
+      <input
+        type="number"
+        value={data.count}
+        onChange={(e) => onSetCount(parseInt(e.target.value))}
+        className="w-[3ch] text-center"
+      />
     </div>
   );
 }
@@ -29,7 +40,7 @@ function Cart() {
 
   let total = 0;
   cart.forEach((cartItem, i) => {
-    total += cartItem.count * cartItem.game.price
+    total += cartItem.count * cartItem.game.price;
   });
 
   const updateCart = () => {
@@ -39,7 +50,7 @@ function Cart() {
   const onBuy = (e: React.FormEvent) => {
     e.preventDefault();
     const target = e.target as typeof e.target & {
-      confirm: { value?: boolean },
+      confirm: { value?: boolean };
     };
 
     if (target.confirm.value && cart.length !== 0) {
@@ -48,25 +59,29 @@ function Cart() {
       navigate("/account");
     }
   };
-  
+
   return (
-    <main className="flex flex-row gap-2">
-      <div className="flex flex-col flex-grow">
+    <main className="flex flex-row justify-center m-4">
+      <div className="mr-4">
         {cart.map((cartItem, i) => (
           <CartItemCard key={i} data={cartItem} updateCart={updateCart} />
         ))}
       </div>
 
       <form onSubmit={onBuy} className="bg-white flex flex-col self-start p-3">
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row">
           <span className="flex-grow">Total:</span>
           <span>${total.toFixed(2)}</span>
         </div>
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row">
           <span>Do you accept the purchase?</span>
           <input id="confirm" name="confirm" type="checkbox" />
         </div>
-        <input type="submit" value="Buy" />
+        <input
+          type="submit"
+          value="Buy"
+          className="border-2 bg-amber-500 text-white hover:border-amber-500 hover:bg-white hover:text-amber-500"
+        />
       </form>
     </main>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,13 +9,17 @@ function Login() {
 
   const onRegister = (e: React.FormEvent) => {
     const target = e.target as typeof e.target & {
-      username: { value?: string },
-      email: { value?: string },
-      password: { value?: string },
-    }; 
+      username: { value?: string };
+      email: { value?: string };
+      password: { value?: string };
+    };
 
-    if (target.username.value && target.email.value && target.password.value && 
-        register(target.username.value, target.email.value, target.password.value)) {
+    if (
+      target.username.value &&
+      target.email.value &&
+      target.password.value &&
+      register(target.username.value, target.email.value, target.password.value)
+    ) {
       navigate("/");
     } else {
       setHasError(true);
@@ -24,12 +28,15 @@ function Login() {
 
   const onLogin = (e: React.FormEvent) => {
     const target = e.target as typeof e.target & {
-      email: { value?: string },
-      password: { value?: string },
+      email: { value?: string };
+      password: { value?: string };
     };
 
-    if (target.email.value && target.password.value && 
-        login(target.email.value, target.password.value)) {
+    if (
+      target.email.value &&
+      target.password.value &&
+      login(target.email.value, target.password.value)
+    ) {
       navigate("/");
     } else {
       setHasError(true);
@@ -46,9 +53,11 @@ function Login() {
   };
 
   return (
-    <main className="absolute top-[50%] left-[50%]
+    <main
+      className="absolute top-[50%] left-[50%]
                      -translate-x-[50%] -translate-y-[50%]
-                     w-max h-max p-16 bg-[#d9d9d9]">
+                     w-max h-max p-16 bg-[#d9d9d9]"
+    >
       <h1 className="text-center text-2xl font-bold">
         {isRegistering ? "Create new account" : "Login to your account"}
       </h1>
@@ -57,39 +66,58 @@ function Login() {
         {isRegistering && (
           <>
             <label htmlFor="username">Username</label>
-            <input id="username" name="username" type="text"
-                   className="mb-5 rounded-sm 
-                             border-[#a7a7a7] border-solid border-2" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              className="mb-5 rounded-sm 
+                             border-[#a7a7a7] border-solid border-2"
+            />
           </>
         )}
-        
+
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email"
-               className="mb-5 rounded-sm 
-                        border-[#a7a7a7] border-solid border-2" />
-        
+        <input
+          id="email"
+          name="email"
+          type="email"
+          className="mb-5 rounded-sm 
+                        border-[#a7a7a7] border-solid border-2"
+        />
+
         <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password"
-               className="mb-5 rounded-sm 
-                        border-[#a7a7a7] border-solid border-2" />
-        
-        <input type="submit" value={isRegistering ? "Register" : "Login"}
-               className="bg-[#707070] rounded-md p-2 text-white font-bold" />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          className="mb-5 rounded-sm 
+                        border-[#a7a7a7] border-solid border-2"
+        />
+
+        <input
+          type="submit"
+          value={isRegistering ? "Register" : "Login"}
+          className="bg-amber-500 rounded-md p-2 text-white font-bold hover:bg-white hover:text-amber-500 hover:border-amber-500 border-2"
+        />
         {hasError && (
           <span className="text-sm max-w-56 mx-auto text-red-600 font-bold">
-            {isRegistering ? "A user with those credentials may already exist"
-                           : "A user with those credentials does not exist"}
+            {isRegistering
+              ? "A user with those credentials may already exist"
+              : "A user with those credentials does not exist"}
           </span>
         )}
       </form>
 
-      <p onClick={() => {
-        setIsRegistering(!isRegistering)
-        setHasError(false);
-      }}
-         className="text-sm text-center cursor-pointer underline text-[#787878]">
-        {isRegistering ? "Already have an account? Sign in here!"
-                      : "Don't have an account? Sign up here!"}
+      <p
+        onClick={() => {
+          setIsRegistering(!isRegistering);
+          setHasError(false);
+        }}
+        className="text-sm text-center cursor-pointer underline text-[#787878]"
+      >
+        {isRegistering
+          ? "Already have an account? Sign in here!"
+          : "Don't have an account? Sign up here!"}
       </p>
     </main>
   );
