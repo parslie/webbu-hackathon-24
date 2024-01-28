3d733bc7404dfa785da44a623dbf86c4926ed2d6
Viktor Holta <viktor.holta@outlook.com>
2024-01-28 09:39:33 +0100

    Create fake cart API

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -2,7 +2,13 @@ import { Game } from "./games";
 
 const CART_KEY = "";
 
-type Cart = Game[];
+type CartItem = {
+  id: number,
+  game: Game,
+  count: number,
+};
+
+type Cart = CartItem[];
 
 function getCart() : Cart {
   const cartStr = localStorage.getItem(CART_KEY);
@@ -14,4 +20,66 @@ function getCart() : Cart {
   return cart;
 }
 
-export { getCart, type Cart };
+function addToCart(game: Game) {
+  let cart = getCart();
+
+  let gameInCart = false;
+  for (let i = 0; i < cart.length; i++) {
+    let cartItem = cart[i];
+    if (cartItem.game.id === game.id) {
+      cartItem.count++;
+      cart[i] = cartItem;
+      gameInCart = true;
+      break;
+    }
+  }
+
+  if (!gameInCart) {
+    const cartItem: CartItem = {
+      id: cart.length,
+      game: game,
+      count: 1,
+    };
+    cart.push(cartItem);
+  }
+  
+  const cartStr = JSON.stringify(cart);
+  localStorage.setItem(CART_KEY, cartStr);
+}
+
+function removeFromCart(game: Game) {
+  let cart = getCart();
+
+  for (let i = 0; i < cart.length; i++) {
+    let cartItem = cart[i];
+    if (cartItem.game.id === game.id) {
+      cart.splice(i, 1);
+      break;
+    }
+  }
+
+  const cartStr = JSON.stringify(cart);
+  localStorage.setItem(CART_KEY, cartStr);
+}
+
+function setCartItemCount(game: Game, count: number) {
+  if (count <= 0) {
+    removeFromCart(game);
+  } else {
+    let cart = getCart();
+
+    for (let i = 0; i < cart.length; i++) {
+      let cartItem = cart[i];
+      if (cartItem.game.id === game.id) {
+        cartItem.count = count;
+        cart[i] = cartItem;
+        break;
+      }
+    }
+
+    const cartStr = JSON.stringify(cart);
+    localStorage.setItem(CART_KEY, cartStr);
+  }
+}
+
+export { getCart, type Cart, type CartItem };
